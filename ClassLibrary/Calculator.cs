b1df0b0eb6e17601db53ca1062326445dff34034
jamesab <james.brookshire@gmail.com>
2015-06-29 16:30:40 -0700

    Simple Calculator Unit Testing
    Simple Calculator Unit Testing

---
+++ ClassLibrary/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+
+        }
+
+        public int Add(int first,int second)
+        {
+            return first + second;
+        }
+
+        public int Subtract(int first, int second)
+        {
+            return first - second;
+        }
+
+        public int Multiply(int first, int second)
+        {
+            return first * second;
+        }
+
+        public int Divide(int first, int second)
+        {
+            return first / second;
+        }
+    }
+}
---
+++ UnitTesting/CalculatorTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        Calculator cal = new Calculator();
+
+        [TestMethod]
+        public void SimpleAdd()
+        {            
+            int results = cal.Add(2, 2);
+            Assert.AreEqual(4, results);
+        }
+
+        [TestMethod]
+        public void SimpleSubtract()
+        {            
+            int results = cal.Subtract(2, 2);
+            Assert.AreEqual(0, results);
+        }
+
+        [TestMethod]
+        public void SimpleMultiplication()
+        {
+            int results = cal.Multiply(2, 2);
+            Assert.AreEqual(4, results);
+        }
+
+        [TestMethod]
+        public void SimpleDivision()
+        {
+            int results = cal.Divide(2, 2);
+            Assert.AreEqual(1, results);
+        }
+    }
+}
